8373b2df0e3b39cc9d6e25d9c873dca82418a1c4
TheRealJackiBoi <jslam@oulund.dk>
2024-01-31 02:06:36 +0100

    week01 basic done

--- week01/src/basicex.js
+++ week01/src/basicex.js
@@ -13,6 +13,17 @@ var __extends = (this && this.__extends) || (function () {
         d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
     };
 })();
+var __assign = (this && this.__assign) || function () {
+    __assign = Object.assign || function(t) {
+        for (var s, i = 1, n = arguments.length; i < n; i++) {
+            s = arguments[i];
+            for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
+                t[p] = s[p];
+        }
+        return t;
+    };
+    return __assign.apply(this, arguments);
+};
 // 1
 var num = 2;
 var str = "Hello";
@@ -65,3 +76,134 @@ var julius = new Employee("Julius", "julleren@gmail.com", 24, 1000);
 // 4
 var anyStr = any;
 var specificStr = anyStr;
+// 5
+var add = function (a, b) { return a + b; };
+// 6
+var http = [200, "OK"];
+var http2 = [400, "error"];
+var http3 = [404, "notfound"];
+var http4 = [500, "server error"];
+var personTuple = ["julius", 24, "julleren@gmail.com"];
+// 7 
+var numOrStr = function (val) {
+    console.log(val);
+};
+var newPers = ["julius", "24", "julleren@gmail.com"];
+console.log(numOrStr("9"));
+// 8
+function takeGenArr(arr) {
+    if (arr.length > 0) {
+        return arr[0];
+    }
+    else {
+        return null;
+    }
+}
+function take2Obj(first, second) {
+    return __assign(__assign({}, first), second);
+}
+// 9
+var arrOfNum = [1, 2, 3, 4, 5];
+var multiDArr = [["-", "-", "-"], ["-", "-", "-"], ["-", "-", "-"]];
+// 10
+// Part 1
+// A variable that might be null or undefined
+var nullableValue = "Hello";
+// Use the exclamation mark to assert that the value is non-null
+var nonNullableValue = nullableValue;
+console.log(nonNullableValue); // Output: Hello
+// Part 2
+// A variable that might be null or undefined
+var myString = "placeholder because of function not defined"; //possibleUndefinedStringFunction();
+// Use the exclamation mark to assert that the value is non-null
+var lemgth = myString.length;
+// 11
+// Part 1
+// A function that takes an optional parameter
+function printName(name) {
+    console.log(name);
+}
+// Call the function without a parameter
+printName(); // Output: undefined
+// Call the function with a parameter
+printName("John"); // Output: John
+// Create a person object with an age property
+// Create a person object without an age property
+var prsn = {
+    name: "Julius"
+};
+// 12
+var numOrStrFunc = function (val) {
+    if (typeof val === "string") {
+        return val;
+    }
+    // could use if/else, but by having the union type of string or number and checking if val is a string, it can only be a number
+    else {
+        return val * 2;
+    }
+};
+// 13
+var anyVar = 2323;
+var myAnyToStr = anyVar;
+var myAnyToStrUseAngle = anyVar;
+// 14
+var direction = function (direction) {
+    switch (direction) {
+        case "left":
+            return 1;
+        case "right":
+            return 2;
+        case "up":
+            return 3;
+        case "down":
+            return 4;
+    }
+};
+var humOrAli = function (creator) {
+    if ("eat" in creator) {
+        return creator.eat;
+    }
+    else {
+        return creator.fly;
+    }
+};
+// 16
+var PersonForCar = /** @class */ (function () {
+    function PersonForCar(name) {
+        this.name = name;
+    }
+    return PersonForCar;
+}());
+var Car = /** @class */ (function () {
+    function Car(model) {
+        this.model = model;
+    }
+    return Car;
+}());
+// instance of only works if it's of a class, and that is type or interface NOT
+// Therefore I've changed Car and PersonForCar to class
+var carOrPerson = function (instance) {
+    if (instance instanceof Car) {
+        return instance.model;
+    }
+    else {
+        return instance.name;
+    }
+};
+// write a type predicate to narrow the type of the fish parameter
+var isFish = function (animal) {
+    return animal.swim !== undefined;
+};
+function howToMove(pet) {
+    if (isFish(pet)) {
+        pet.swim();
+    }
+    else {
+        pet.fly();
+    }
+}
+var persFromInterface = {
+    name: "Jack",
+    "age": 21
+};
+var combineStudentAndPerson = function (person, student) { return __assign(__assign({}, person), student); };
--- week01/src/basicex.ts
+++ week01/src/basicex.ts
@@ -87,8 +87,13 @@ console.log(numOrStr("9"))
 
 
 // 8
-function takeGenArr <T> (arr: T[]): T { 
-  return arr.length > 0 ? arr[0] : undefined
+function takeGenArr <T> (arr: T[]): T | null { 
+  if (arr.length > 0) { 
+    return arr[0]
+  } 
+  else {
+    return null
+  }
 }
 
 
@@ -97,6 +102,183 @@ function take2Obj <T extends object, U extends object> (first: T, second: U) {
 }
 
 
+// 9
+const arrOfNum: Array<number> = [1, 2, 3, 4, 5]
+
+const multiDArr: Array<Array<string>> = [["-", "-", "-"], ["-", "-", "-"], ["-", "-", "-"]]
+
+
+// 10
+ // Part 1
+ // A variable that might be null or undefined
+ let nullableValue: string | null | undefined = "Hello";
+
+ // Use the exclamation mark to assert that the value is non-null
+ let nonNullableValue: string = nullableValue!;
+
+ console.log(nonNullableValue!); // Output: Hello
+
+// Part 2
+// A variable that might be null or undefined
+let myString: string | undefined = "placeholder because of function not defined"//possibleUndefinedStringFunction();
+// Use the exclamation mark to assert that the value is non-null
+let lemgth: number = myString!.length; 
+
+
+// 11
+// Part 1
+// A function that takes an optional parameter
+function printName(name?: string) {
+  console.log(name);
+}
+
+// Call the function without a parameter
+printName(); // Output: undefined
+// Call the function with a parameter
+printName("John"); // Output: John
+
+// Part 2
+// A type alias with an optional age property
+type NewPersonType = {
+  name: string;
+  age?: number;
+};
+
+// Create a person object with an age property
+// Create a person object without an age property
+const prsn: NewPersonType = {
+  name: "Julius"
+}
 
 
 
+// 12
+const numOrStrFunc = (val: number | string): string | number => {
+  if ( typeof val === "string" ) {
+    return val
+  }
+  // could use if/else, but by having the union type of string or number and checking if val is a string, it can only be a number
+  else {
+    return val * 2
+  }
+}
+
+
+// 13
+const anyVar: any = 2323
+const myAnyToStr = anyVar as string
+const myAnyToStrUseAngle = <string>anyVar
+
+
+// 14
+const direction = (direction: "left" | "right" | "up" | "down"): number => {
+
+  switch (direction) {
+    case "left":
+      return 1
+    case "right":
+      return 2
+    case "up":
+      return 3
+    case "down":
+      return 4
+  }
+  
+}
+
+
+// 15
+type Human = {
+  eat: Function
+}
+
+type Alien = {
+  fly: Function
+}
+
+
+const humOrAli = (creator: Human | Alien): Function => {
+  if ("eat" in creator) {
+    return creator.eat
+  }
+  else {
+    return creator.fly
+  }
+}
+
+
+// 16
+class PersonForCar {
+  name: string
+  constructor(name: string) {
+    this.name = name
+  }
+}
+
+class Car {
+  model: string
+  constructor(model: string) {
+    this.model = model
+  }
+}
+
+// instance of only works if it's of a class, and that is type or interface NOT
+// Therefore I've changed Car and PersonForCar to class
+const carOrPerson = (instance: Car | PersonForCar): string => {
+  if (instance instanceof Car) {
+    return (instance as Car).model
+  }
+  else {
+    return (instance as PersonForCar).name
+  }
+}
+
+
+// 17
+interface Bird {
+  fly(): void;
+  layEggs(): void;
+}
+
+interface Fish {
+  swim(): void;
+  layEggs(): void;
+}
+
+// write a type predicate to narrow the type of the fish parameter
+const isFish = (animal: Fish | Bird): animal is Fish => {
+  return (animal as Fish).swim !== undefined
+}
+
+function howToMove(pet: Fish | Bird) {
+  if (isFish(pet)) {
+    pet.swim();
+  } else {
+    pet.fly();
+  }
+}
+
+
+
+// 18
+interface PersInterface {
+  name: string
+  [key: string]: string | number
+}
+
+const persFromInterface: PersInterface = {
+  name: "Jack",
+  "age": 21
+}
+
+
+// 19
+type SndPersInterface = {
+  name: string
+}
+
+type Student = {
+  studentId: number
+}
+
+const combineStudentAndPerson = (person: SndPersInterface, student: Student): Student & SndPersInterface => { return {...person, ...student}}
