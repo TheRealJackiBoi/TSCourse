8c1207571287171b491b33c4efda92bb1dfdad36
TheRealJackiBoi <jslam@oulund.dk>
2024-02-03 01:25:57 +0100

    minor week01

--- week01/typedeclarationexercise/src/typeDeclaration.ts
+++ week01/typedeclarationexercise/src/typeDeclaration.ts
@@ -16,3 +16,25 @@ axios.get("https://jsonplaceholder.typicode.com/users")
   .then((res: AxiosResponse<User[]>) => console.log(res.data))
   .catch((err: AxiosError) => console.log(err));
 
+
+
+/*
+
+Questions:
+1. Where do you find axios type declarations file?
+
+You can either find it in the node_modules folder or on the github page.
+
+2. What is the name of the type declaration file for axios?
+
+index.d.ts
+
+3. How do you install a type declaration fille for a library that does not have one?
+
+You can install it using npm install @types/libraryname
+
+4. What is the difference between a type declaration fifle and a type definition file?
+
+A type declaration file is a file that contains type declarations. A type definition file is a file that contains type definitions. They are the basicly the same thing.
+
+*/
